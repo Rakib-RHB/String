71f27ea3c00cfc11eb55e2789f2992c066e4fb8f
Rakib1977 <140387583+Rakib1977@users.noreply.github.com>
2023-09-15 23:51:37 +0600

    Extract a substring from a given string .c
    . Write a program in C to extract a substring from a given string.
    
    Test Data :
    Input the string : this is test string
    Input the position to start extraction :9
    Input the length of substring :4
    
    Expected Output :
    
    The substring retrieve from the string is : " test "

---
+++ Extract a substring from a given string.c
@@ -0,0 +1,44 @@
+/*Write a program in C to extract a substring from a given string.
+
+Test Data :
+Input the string : this is test string
+Input the position to start extraction :9
+Input the length of substring :4
+
+Expected Output :
+
+The substring retrieve from the string is : " t*/
+
+
+
+
+#include <stdio.h>
+void main()
+{
+    char str[100], sstr[100];
+    int pos, l, c = 0;
+
+    printf("\n\nExtract a substring from a given string:\n");
+    printf("--------------------------------------------\n");
+
+    printf("Input the string : ");
+    fgets(str, sizeof str, stdin);
+
+    printf("Input the position to start extraction :");
+    scanf("%d", &pos);
+
+    printf("Input the length of substring :");
+    scanf("%d", &l);
+
+    while (c < l)
+    {
+        sstr[c] = str[pos+c-1];
+        c++;
+    }
+    sstr[c] = '\0';
+
+    printf("The substring retrieve from the string is : \" %s\ "\n\n", sstr);
+
+}
+
+
